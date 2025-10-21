59b3c76f2f3ca8546882a3111c735eb2044b4190
utreah <ersoyugur1528@gmail.com>
2025-10-21 23:41:25 +0100

    changes on game width, added gravity for collision, added platforms array and removed platforms from layers, moved layer logic to onUpdate function, added loop function to generate pillars

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -1,9 +1,10 @@
 import kaplay from "kaplay";
 import kaplay_canvas from "../header"; 
-import { owlet } from "../entities/owlet-monster"
+import { makeOwlet } from "../entities/owlet-monster"
+import { createPillar } from "../entities/pillar";
 export default function game(){
-
-    const IMAGE_WIDTH = 576;       
+    kaplay_canvas.setGravity(3100);
+    const IMAGE_WIDTH = 800;       
     const layers = [
         {
             speed: -15,
@@ -31,16 +32,58 @@ export default function game(){
             ],
         },
     ];
-    owlet(kaplay_canvas.vec2(100, 276));
+    const platforms = [
+            kaplay_canvas.add([kaplay_canvas.sprite("near-field"), kaplay_canvas.pos(0, 0)]),
+            kaplay_canvas.add([kaplay_canvas.sprite("near-field"), kaplay_canvas.pos(384, 450)]),
+
+    ]
+    const owlet = makeOwlet(kaplay_canvas.vec2(100, 276));
+    owlet.setControls();
+    owlet.setEvents();
+    //createPillar(200 , 276);
+    
+    kaplay_canvas.add([
+        kaplay_canvas.rect(600, 100),
+        kaplay_canvas.opacity(0),
+        kaplay_canvas.area(),
+        kaplay_canvas.pos(0, 275),
+        kaplay_canvas.body({isStatic: true}),
+    ])
+    let gameSpeed = 200;
+    kaplay_canvas.loop(1, () => {
+        gameSpeed += 5;
+        kaplay_canvas.add([
+            kaplay_canvas.sprite("pillar"),
+            kaplay_canvas.scale(),
+            kaplay_canvas.pos(400, 215),
+            //kaplay_canvas.area(),
+            //kaplay_canvas.body(),
+            kaplay_canvas.outline(),
+            kaplay_canvas.move(kaplay_canvas.vec2(-1, 0), gameSpeed - 20),
+            kaplay_canvas.body({isStatic: true})
+
+        ])
+    });
     kaplay_canvas.onUpdate(() => {
         for(const layer of layers){
             if(layer.parts[1].pos.x < 0){
                 layer.parts[0].moveTo(layer.parts[1].pos.x + IMAGE_WIDTH, 0);
                 layer.parts.push(layer.parts.shift());
-            }
-            layer.parts[0].move(layer.speed, 0);
-            layer.parts[1].move(layer.speed, 0);
+             }
+        layer.parts[0].move(-100, 0);
+        layer.parts[1].moveTo(layer.parts[0].pos.x + IMAGE_WIDTH - 230, 0);
+    }
+        
+
+
+        if(platforms[1].pos.x < 0){
+            platforms[0].moveTo(platforms[1].pos.x + IMAGE_WIDTH, 0);
+            platforms.push(platforms.shift());
         }
+        platforms[0].move(-gameSpeed, 0);
+        platforms[1].moveTo(platforms[0].pos.x + platforms[1].width, 0);
+
+
     })
     //kaplay_canvas.add([kaplay_canvas.sprite("owlet-monster"), kaplay_canvas.pos(100, 276)], kaplay_canvas.rotate(1000));
 }
