5111795a133fbc0b3f7373b3bf513185bcd7af92
utreah <ersoyugur1528@gmail.com>
2025-10-21 23:39:21 +0100

    added setControls and setEvents function, updated function name, added actor jumpForce

--- src/entities/owlet-monster.js
+++ src/entities/owlet-monster.js
@@ -1,12 +1,28 @@
 import kaplay_canvas from "../header";
 
-export function owlet(pos){
-    const actor_owlet = kaplay_canvas.add([
+export function makeOwlet(pos){
+    const owlet = kaplay_canvas.add([
         kaplay_canvas.sprite("owlet-monster", {anim: "run"}),
         kaplay_canvas.scale(1.4),
         kaplay_canvas.area(),
         kaplay_canvas.anchor("botleft"),
         kaplay_canvas.pos(pos),
-
+        kaplay_canvas.body({jumpForce: 1000}),
+        {
+            setControls(){
+                kaplay_canvas.onButtonPress("jump", () => {
+                    if(this.isGrounded()){
+                        this.play("jump");
+                        this.jump();
+                    }
+                });
+            },
+            setEvents(){
+                this.onGround(() => {
+                    this.play("run");
+                });
+            }
+        }
     ]);
+    return owlet;
 }
