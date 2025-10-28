a1d42deb6a0beeb6241962f3a24dcb089511d423
utreah <ersoyugur1528@gmail.com>
2025-10-28 16:40:01 +0000

    implemented login system

---
+++ document1/login.js
@@ -0,0 +1,25 @@
+document.getElementById("loginButton").addEventListener("click", ()=>{
+    const username = document.getElementById("loginUsername").value;
+    const password = document.getElementById("loginPassword").value;
+
+    if(!username || !password){
+        alert("Username/Password fields must be filled!");
+        return;
+    }
+    let users = JSON.parse(localStorage.getItem("users"));
+    if(!users){
+        alert("No account has  been found!");
+        users = [];
+        return;
+    }
+    for(let i = 0; i < users.length; i++){
+        if(users[i].username === username && users[i].password === password){
+            document.getElementById("loginMessage").textContent = "Login succesful!"
+            localStorage.setItem("loggedUser", JSON.stringify(users[i]));
+            window.location.href = "index.html"
+        }
+    }
+
+    if(!localStorage.getItem("loggedUser"))
+        document.getElementById("loginMessage").textContent = "Wrong username or password!";
+})
