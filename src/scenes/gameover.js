a4a5dd8256b87cb2b8950b6cdabb1399364ac990
utreah <ersoyugur1528@gmail.com>
2025-10-28 16:49:39 +0000

    best score shows loggedUser's best score,  added a logic to set high score if session score is higher than high score

--- src/scenes/gameover.js
+++ src/scenes/gameover.js
@@ -1,14 +1,33 @@
 import kaplay_canvas from "../header";
 import game from "./game";
-
+import mainMenu from "./mainMenu";
 export default function gameOver(){
+    let loggedUser = JSON.parse(localStorage.getItem("loggedUser"));
+    let users = JSON.parse(localStorage.getItem("users")) || [];
+    let sessionScore = kaplay_canvas.getData("session-score");
+
     if(kaplay_canvas.getData("best-score") == null)
         kaplay_canvas.setData("best-score", 0);
         kaplay_canvas.add([kaplay_canvas.text("YOU DIED!"), {font: "slkscr"}, kaplay_canvas.pos(kaplay_canvas.center()), kaplay_canvas.anchor("center")]);
-        kaplay_canvas.add([kaplay_canvas.text("SCORE: " + kaplay_canvas.getData("score")), { size: 0 , font: "slkscr"}, kaplay_canvas.pos(0, 0)]);
-        kaplay_canvas.add([kaplay_canvas.text("BEST SCORE: " + kaplay_canvas.getData("best-score")), { size: 0 , font: "slkscr"}, kaplay_canvas.pos(0, 50)]);
+        kaplay_canvas.add([kaplay_canvas.text("SCORE: " + kaplay_canvas.getData("session-score")), { size: 0 , font: "slkscr"}, kaplay_canvas.pos(0, 0)]);
+        kaplay_canvas.add([kaplay_canvas.text("BEST SCORE: " + loggedUser.bestScore), { size: 0 , font: "slkscr"}, kaplay_canvas.pos(0, 50)]);
         kaplay_canvas.onButtonPress("jump", () => {
-            kaplay_canvas.go("game", game);
+            kaplay_canvas.go("main-menu", mainMenu);
         })
-        
+
+    if(loggedUser){
+        if(sessionScore > loggedUser.bestScore){
+            loggedUser.bestScore = sessionScore;
+
+            for(let i = 0; i < users.length; i++){
+                if(users[i].username === loggedUser.username){
+                    users[i].bestScore = sessionScore;
+                    break;
+                }
+            }
+
+            localStorage.setItem("loggedUser", JSON.stringify(loggedUser));
+            localStorage.setItem("users", JSON.stringify(users));
+        }
+    }
 }
